1f62e0bd7d206c13d4463ae8a2ce5933319044e3
sserges <agounkeserge@gmail.com>
2022-02-17 18:56:49 -0500

    Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,20 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
+
+const User = require('../models/userModel')
 
 // @desc Register new User
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+
   res.json({ message: 'Register User' })
 })
 
