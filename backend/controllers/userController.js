96f9c87bc8eedc8a0ef25c71424ed31fd4e7cdf1
sserges <agounkeserge@gmail.com>
2022-02-21 19:02:43 -0500

    Generate JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,6 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user.id),
     })
   } else {
     res.status(400)
@@ -62,6 +63,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user.id),
     })
   } else {
     res.status(400)
@@ -75,6 +77,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'User data' })
 })
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
