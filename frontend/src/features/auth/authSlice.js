8db8fb66de591d47387159dfa8d991df6f0e80cd
sserges <agounkeserge@gmail.com>
2022-03-18 19:43:30 -0400

    Start User Registration

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
+import authService from './authService'
+
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
 
@@ -10,6 +12,23 @@ const initialState = {
   isLoading: false,
   message: '',
 }
+
+// Register user
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user)
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
 
 export const authSlice = createSlice({
   name: 'auth',
