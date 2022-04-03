b56ccd5f5cfb94c5fb567feb9a27d8a01d65dd1c
sserges <agounkeserge@gmail.com>
2022-04-03 14:25:52 -0400

    Dashboard & Goal Form

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
+import GoalForm from '../components/GoalForm'
+
 function Dashboard() {
     const navigate = useNavigate()
 
@@ -14,7 +16,14 @@ function Dashboard() {
     }, [user, navigate])
 
     return (
-        <div>Dashboard</div>
+        <>
+            <section className="heading">
+                <h1>Welcome {user && user.name}</h1>
+                <p>Goals Dashboard</p>
+            </section>
+
+            <GoalForm />
+        </>
     )
 }
 
